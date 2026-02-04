b4b4bcc6f0efc7c16501e768352449e98510e115
Sigsign <sig@signote.cc>
2026-02-04 04:23:57 +0000

    Refactor Translate()

--- translator.go
+++ translator.go
@@ -2,9 +2,9 @@ package bfconv
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
-	ext "github.com/mmcdole/gofeed/extensions"
 	"github.com/mmcdole/gofeed/rss"
 )
 
@@ -34,16 +34,16 @@ func (t *Translator) Translate(rss *rss.Feed) (*Feed, error) {
 		item.URL = r.Link
 		item.Title = r.Title
 		item.ContentText = r.Description
-		item.Image = t.translateItemImage(r)
+		item.Image = t.extractExtensionValue(r, "imageurl")
 		item.DatePublished = t.translateItemDatePublished(r)
 		item.Tags = t.translateItemTags(r)
 
-		bookmarkExt := &BookmarkExtension{}
-		bookmarkExt.Count = t.translateBookmarkCount(r)
-		bookmarkExt.CommentListPageURL = t.translateBookmarkCommentListPageURL(r)
-		bookmarkExt.SiteEntriesListURL = t.translateBookmarkSiteEntriesListURL(r)
+		ext := &BookmarkExtension{}
+		ext.Count = t.translateBookmarkCount(r)
+		ext.CommentListPageURL = t.extractExtensionValue(r, "bookmarkCommentListPageUrl")
+		ext.SiteEntriesListURL = t.extractExtensionValue(r, "bookmarkSiteEntriesListUrl")
 
-		item.BookmarkExt = bookmarkExt
+		item.BookmarkExt = ext
 
 		items = append(items, item)
 	}
@@ -52,12 +52,12 @@ func (t *Translator) Translate(rss *rss.Feed) (*Feed, error) {
 	return &feed, nil
 }
 
-func (t *Translator) translateItemDatePublished(rssItem *rss.Item) string {
-	if rssItem == nil || rssItem.DublinCoreExt == nil || len(rssItem.DublinCoreExt.Date) == 0 {
+func (t *Translator) translateItemDatePublished(item *rss.Item) string {
+	if item == nil || item.DublinCoreExt == nil || len(item.DublinCoreExt.Date) == 0 {
 		return ""
 	}
 
-	date := rssItem.DublinCoreExt.Date[0]
+	date := item.DublinCoreExt.Date[0]
 	if _, err := time.Parse(time.RFC3339, date); err == nil {
 		return date
 	}
@@ -65,75 +65,53 @@ func (t *Translator) translateItemDatePublished(rssItem *rss.Item) string {
 	return ""
 }
 
-func (t *Translator) translateItemImage(rssItem *rss.Item) string {
-	imageURLs := t.extractExtension(rssItem, "hatena", "imageurl")
-	if len(imageURLs) == 0 {
-		return ""
-	}
-
-	image := imageURLs[0].Value
-	return image
-}
-
-func (t *Translator) translateItemTags(rssItem *rss.Item) []string {
-	subjects := t.extractExtension(rssItem, "dc", "subject")
-	if len(subjects) == 0 {
+func (t *Translator) translateItemTags(item *rss.Item) []string {
+	if item == nil {
 		return []string{}
 	}
 
-	tags := make([]string, 0, len(subjects))
-	for _, s := range subjects {
-		tags = append(tags, s.Value)
+	elems := item.Extensions["dc"]
+	if len(elems) == 0 {
+		return []string{}
+	}
+
+	values := elems["subject"]
+	if len(values) == 0 {
+		return []string{}
+	}
+
+	tags := make([]string, 0, len(values))
+	for _, v := range values {
+		tags = append(tags, v.Value)
 	}
 
 	return tags
 }
 
-func (t *Translator) translateBookmarkCount(rssItem *rss.Item) int {
-	counts := t.extractExtension(rssItem, "hatena", "bookmarkcount")
-	if len(counts) == 0 {
+func (t *Translator) translateBookmarkCount(item *rss.Item) int {
+	c := t.extractExtensionValue(item, "bookmarkcount")
+	n, err := strconv.Atoi(c)
+	if err != nil {
 		return 0
 	}
 
-	var n int
-	fmt.Sscanf(counts[0].Value, "%d", &n)
 	return n
 }
 
-func (t *Translator) translateBookmarkCommentListPageURL(rssItem *rss.Item) string {
-	URLs := t.extractExtension(rssItem, "hatena", "bookmarkCommentListPageUrl")
-	if len(URLs) == 0 {
+func (t *Translator) extractExtensionValue(item *rss.Item, name string) string {
+	if item == nil {
 		return ""
 	}
 
-	url := URLs[0].Value
-	return url
-}
-
-func (t *Translator) translateBookmarkSiteEntriesListURL(rssItem *rss.Item) string {
-	URLs := t.extractExtension(rssItem, "hatena", "bookmarkSiteEntriesListUrl")
-	if len(URLs) == 0 {
+	elems := item.Extensions["hatena"]
+	if len(elems) == 0 {
 		return ""
 	}
 
-	url := URLs[0].Value
-	return url
-}
-
-func (t *Translator) extractExtension(rssItem *rss.Item, ns string, name string) []ext.Extension {
-	if rssItem == nil || len(rssItem.Extensions) == 0 {
-		return nil
+	values := elems[name]
+	if len(values) == 0 {
+		return ""
 	}
 
-	elms, ok := rssItem.Extensions[ns]
-	if !ok || len(elms) == 0 {
-		return nil
-	}
-
-	items, ok := elms[name]
-	if !ok || len(items) == 0 {
-		return nil
-	}
-
-	return items
+	return values[0].Value
 }
